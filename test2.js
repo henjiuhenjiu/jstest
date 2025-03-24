7e9614520103ed08770dee9e0ec9fc2421f0003b
很久很久 <1835775922@qq.com>
2025-03-24 23:17:39 +0800

    test2.js

---
+++ test2.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         swag(R站)vip视频随意观看&19J.TV韩国女主播vip视频任意看 永久vip
+// @name:zh-TW   swag(R站)vip視頻隨意觀看&19J.TV韓國女主播vip視頻任意看 永久vip
+// @namespace    http://tampermonkey.net/
+// @version      2.4.0
+// @description  用于观看swag(R站)vip视频&19J.TV韩国女主播vip视频
+// @description:zh-TW  用于观看swag(R站)vip視頻&19J.TV韓國女主播vip視頻
+// @author       FFFFFFeng
+// @match        https://*/*
+// @match        http://*/*
+// @icon         https://www.google.com/s2/favicons?domain=swag555.xyz
+// @grant        none
+// @antifeature payment
+// @downloadURL https://update.sleazyfork.org/scripts/440139/swag%28R%E7%AB%99%29vip%E8%A7%86%E9%A2%91%E9%9A%8F%E6%84%8F%E8%A7%82%E7%9C%8B19JTV%E9%9F%A9%E5%9B%BD%E5%A5%B3%E4%B8%BB%E6%92%ADvip%E8%A7%86%E9%A2%91%E4%BB%BB%E6%84%8F%E7%9C%8B%20%E6%B0%B8%E4%B9%85vip.user.js
+// @updateURL https://update.sleazyfork.org/scripts/440139/swag%28R%E7%AB%99%29vip%E8%A7%86%E9%A2%91%E9%9A%8F%E6%84%8F%E8%A7%82%E7%9C%8B19JTV%E9%9F%A9%E5%9B%BD%E5%A5%B3%E4%B8%BB%E6%92%ADvip%E8%A7%86%E9%A2%91%E4%BB%BB%E6%84%8F%E7%9C%8B%20%E6%B0%B8%E4%B9%85vip.meta.js
+// ==/UserScript==
+(function () {
+    let kitty_emojiList = ["😃", "😄", "😁", "😆", "😅", "🤣", "😂", "🙂", "🙃", "🫠", "😉", "😊", "😇", "😕", "🫤", "😟", "🙁", "☹", "😯", "😲", "🥺", "🥹", "😦", "😧", "😨", "😰", "😥", "😢", "😭", "😱", "😖", "😣", "😞", "😓", "😩", "😫", "😤", "😡", "😠"]
+    let kitty_emojiIndex = 0
+    let kitty_StatusColor = {
+        success: "#67C23A",
+        warning: "#E6A23C",
+        info: "#909399",
+        danger: "#F56C6C",
+        '200': "#67C23A",
+        '300': "#E6A23C",
+        '400': "#909399",
+        '500': '#F56C6C'
+    }
+})();
